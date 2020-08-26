14cf23b39b271ab27b1733625473b3c8523e9fc5
Royer <A01209400@itesm.mx>
2020-08-26 17:51:35 -0500

    add shopping list

--- Lab05/myJS.js
+++ Lab05/myJS.js
@@ -1,10 +1,4 @@
 function verifyPasswordRequierements(){
-    let lengthPass=false; //must contain at least 8 characters.
-    let upperCase=false; //must contain at least one uppercase. from 65 to 90 ascii table
-    let loweCase=false; //must contain at least one lowecase. from 97 to 122 ascii table
-    let numeber=false; //must contain at least one number. from 48 to 57 ascii table
-    let specialChar=false; //must contain at least one special character. from 33 to 63 ascii table
-
     let password=document.getElementById("Password").value;
     const passw=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
     if(password.match(passw)){
@@ -20,8 +14,40 @@ function verifyPasswordRequierements(){
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
 
+function suma(id){
+    
+    let counter=document.getElementById(id).value;
+    counter=parseInt(counter)
+    counter+=1;
+    document.getElementById(id).value=(counter);
+}
+
+function resta(id){
+    
+    let counter=document.getElementById(id).value;
+    counter=parseInt(counter)
+    counter-=1;
+    document.getElementById(id).value=(counter);
+}
+
+function generaCuenta(){
+    let total=0;
+    let subtotal=0;
+    let ham=parseInt(document.getElementById("counter1").value);
+    let hot=parseInt(document.getElementById("counter2").value);
+    let fri=parseInt(document.getElementById("counter3").value);
+    let cuenta=document.getElementById("cuenta");
+    let li1 = document.createElement('li');
+    li1.setAttribute("class","collection-item");
+    li1.appendChild(document.createTextNode(ham+" Hambuger             "+ham*80));
+    let li2 = document.createElement('li');
+    li2.setAttribute("class","collection-item");
+    li2.appendChild(document.createTextNode(hot+" Hot-dog             "+hot*80)); 
+    let li3 = document.createElement('li');
+    li3.setAttribute("class","collection-item");
+    li3.appendChild(document.createTextNode(fri+" fries             "+fri*80)); 
+    cuenta.appendChild(li1);
+    cuenta.appendChild(li2);
+    cuenta.appendChild(li3);
+}
