4eb4d5b71d389a641e469f9338a24ea57c3f19e0
Royer Arenas <A01209400@itesm.mx>
2020-08-31 23:27:33 -0500

    parcial genera cuenta

--- Lab07/js/myJS.js
+++ Lab07/js/myJS.js
@@ -1,36 +1,47 @@
 var quesoList = new Array();
 var migajasList = new Array();
 var flag = 0;
+
+function registerTrans(){
+    aux = document.getElementById("counter").value;
+    aux = parseInt(aux)
+    aux = 0;
+    document.getElementById("counter").value = (aux);
+    generaCuenta();
+    obj = document.getElementById("show-guisados").classList.remove("is-active");
+    
+}
+
 function appendToList(string) {
-    if (flag == 0) {
-        quesoList.push(string);
-        console.log(quesoList.length);
-    } else {
-        migajasList.push(string);
+    aux = document.getElementById("counter").value;
+    if(!flag) {
+        if(aux>quesoList.length){
+            quesoList.push(string);
+        }else{
+            alert("No más");
+        }
+    }else{
+        if(aux>migajasList.length){
+            migajasList.push(string);
+        }else{
+            alert("No más");
+        }
     }
 }
 
 function show_Guisados(type) {
     obj = document.getElementById("show-guisados").classList.add("is-active");
-    if (type == '1') {
-        console.log(type);
-        aux = document.getElementById("counter").value
-        console.log(aux);
-        console.log(quesoList.length);
+    if (type == '0') {
         flag = 0;
-        for(let i=0;i<aux;i++) {
-            alert("Agrega guiso");
-        }
     } else {
         flag = 1;
-        console.log("Agrega guisado");
     }
 }
 
 function suma(id) {
 
     let counter = document.getElementById(id).value;
-    counter = parseInt(counter)
+    counter = parseInt(counter);
     counter += 1;
     document.getElementById(id).value = (counter);
 }
@@ -38,32 +49,43 @@ function suma(id) {
 function resta(id) {
 
     let counter = document.getElementById(id).value;
-    counter = parseInt(counter)
+    counter = parseInt(counter);
     counter -= 1;
     document.getElementById(id).value = (counter);
 }
 
-function guisados(type) {
 
+function displayCuenta(){
+    quesos=document.getElementById("noQueso");
+    migajas=document.getElementById("noMigajas");
+    for(let i=0;i<quesoList.length;i++){
+        let li=document.createElement("li");
+        console.log(quesoList[i]);
+        li.appendChild(document.createTextNode(quesoList[i]));
+        quesos.appendChild(li);
+    }
+    for(let i=0;i<migajasList.length;i++){
+        let li=document.createElement("li");
+        console.log(migajasList[i]);
+        li.appendChild(document.createTextNode(migajasList[i]));
+        migajas.appendChild(li);
+    }
 }
 
 function generaCuenta() {
     let total = 0;
     let subtotal = 0;
-    let ham = parseInt(document.getElementById("counter1").value);
-    let hot = parseInt(document.getElementById("counter2").value);
-    let fri = parseInt(document.getElementById("counter3").value);
-    let cuenta = document.getElementById("cuenta");
-    let li1 = document.createElement('li');
-    li1.setAttribute("class", "collection-item");
-    li1.appendChild(document.createTextNode(ham + " Hambuger             " + ham * 80));
-    let li2 = document.createElement('li');
-    li2.setAttribute("class", "collection-item");
-    li2.appendChild(document.createTextNode(hot + " Hot-dog             " + hot * 80));
-    let li3 = document.createElement('li');
-    li3.setAttribute("class", "collection-item");
-    li3.appendChild(document.createTextNode(fri + " fries             " + fri * 80));
-    cuenta.appendChild(li1);
-    cuenta.appendChild(li2);
-    cuenta.appendChild(li3);
+    let envio=false;
+    let totalQueso=0;
+    let totalMigajas=0;
+    
+    totalQueso=quesoList.length*25;
+    totalMigajas=migajasList.length*25;
+    subtotal=totalMigajas+totalMigajas;
+    if(envio){
+        total+=25;
+    }
+    total+=subtotal
+    //console.log(quesoList);
+    displayCuenta();
 }
