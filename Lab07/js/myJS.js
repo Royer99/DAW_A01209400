41539f6c543206e6b00d1b064e4c539b386c8134
Royer Arenas <A01209400@itesm.mx>
2020-09-01 19:38:29 -0500

    fix checkout

--- Lab07/js/myJS.js
+++ Lab07/js/myJS.js
@@ -3,8 +3,7 @@ var migajasList = new Array();
 var flag = 0;
 
 function registerTrans(){
-    aux = document.getElementById("counter").value;
-    aux = parseInt(aux)
+
     aux = 0;
     document.getElementById("counter").value = (aux);
     generaCuenta();
@@ -33,8 +32,12 @@ function show_Guisados(type) {
     obj = document.getElementById("show-guisados").classList.add("is-active");
     if (type == '0') {
         flag = 0;
+        aux=quesoList.length;
+        document.getElementById("counter").value = (aux);
     } else {
         flag = 1;
+        aux=migajasList.length;
+        document.getElementById("counter").value = (aux);
     }
 }
 
@@ -58,21 +61,21 @@ function resta(id) {
 function displayCuenta(){
     quesos=document.getElementById("noQueso");
     migajas=document.getElementById("noMigajas");
+    quesos.innerHTML = "";
+    migajas.innerHTML = "";
     for(let i=0;i<quesoList.length;i++){
         let li=document.createElement("li");
-        console.log(quesoList[i]);
         li.appendChild(document.createTextNode(quesoList[i]));
         quesos.appendChild(li);
     }
     for(let i=0;i<migajasList.length;i++){
         let li=document.createElement("li");
-        console.log(migajasList[i]);
         li.appendChild(document.createTextNode(migajasList[i]));
         migajas.appendChild(li);
     }
 }
 
-function generaCuenta() {
+function generaCuenta(){
     let total = 0;
     let subtotal = 0;
     let envio=false;
@@ -81,11 +84,18 @@ function generaCuenta() {
     
     totalQueso=quesoList.length*25;
     totalMigajas=migajasList.length*25;
-    subtotal=totalMigajas+totalMigajas;
-    if(envio){
+    subtotal=totalQueso+totalMigajas;
+    sub=document.getElementById("subtotal");
+    sub.innerHTML = "";
+    sub.appendChild(document.createTextNode("$"+subtotal));
+    envio=document.getElementById("envio")
+    if(envio.checked==true){
         total+=25;
     }
     total+=subtotal
     //console.log(quesoList);
     displayCuenta();
+    tot=document.getElementById("total");
+    tot.innerHTML="";
+    tot.appendChild(document.createTextNode("$"+total));
 }
