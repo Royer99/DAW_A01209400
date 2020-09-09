5bd4fd44784e5b0ede4d6ce8423990591e2ee727
Royer Arenas <A01209400@itesm.mx>
2020-09-08 20:26:36 -0500

    add access file

--- Lab10/index.php
+++ Lab10/index.php
@@ -2,14 +2,18 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    include_once("_header.html");
     include_once("util.php");
+    include_once("_head.html");
+    include_once("_header.html");
     function main(){
         $dataset1=array();
         $dataset2=array();
         displayContent($dataset1);
         displayContent($dataset2);
         powerTable();
+        include_once("_footer.html");
+        
     }
 main();
+    
 ?>
